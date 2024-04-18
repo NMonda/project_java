6902ce1cc877ec392c476c5be96d24b284e5e9d0
NMONDA <ndegemonda@gmail.com>
2024-04-18 11:37:01 +0300

    file reader and displaying its content

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,20 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-
-
-            System.out.println("Enter a whole number to divide: ");
-            int x = scanner.nextInt();
-
-            System.out.println("Enter a whole number to divide by: ");
-            int y = scanner.nextInt();
-            double z = (double) x / y;
-
-            System.out.println("result: " + z);
-        } catch (ArithmeticException e) {
-            System.out.println("you can't divide by ZERO! Idiot");
-        } catch (InputMismatchException e) {
-            System.out.println("Please enter a number ayee!!!");
-        } catch (Exception e) {
-            System.out.println("Incorrect input! Please enter correct values ");
+        try {
+            FileReader reader = new FileReader("art.txt");
+            int data = reader.read();
+            while (data != -1){
+                System.out.print((char)data);
+                data = reader.read();
+            }
+            reader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        //System.out.println("This will always print");
+
 
     }
 }
