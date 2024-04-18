0d869998c79580340badadd90801d81079c5c1b2
NMONDA <ndegemonda@gmail.com>
2024-04-18 11:35:24 +0300

    file writer

--- src/package2/C.java
+++ src/package2/C.java
@@ -1,7 +1,39 @@
-package package2;
+/*package package2;
 
 public class C {
     public String publicMessage = ("This is public");
     String defaultMessage = "this is the default";
 
 }
+
+ //exception handling code
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+
+
+            System.out.println("Enter a whole number to divide: ");
+            int x = scanner.nextInt();
+
+            System.out.println("Enter a whole number to divide by: ");
+            int y = scanner.nextInt();
+            double z = (double) x / y;
+
+            System.out.println("result: " + z);
+        } catch (ArithmeticException e) {
+            System.out.println("you can't divide by ZERO! Idiot");
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter a number ayee!!!");
+        } catch (Exception e) {
+            System.out.println("Incorrect input! Please enter correct values ");
+        }
+        //System.out.println("This will always print");
+
+    }
+}
+
+ */
+
